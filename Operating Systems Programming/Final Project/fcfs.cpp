785a559c90de8e62d3c39073d84a7e5f972c176f
Gianna Galard <80483712+giannagalard@users.noreply.github.com>
2021-05-10 17:25:05 -0400

    Added Comments

--- Operating Systems Programming/Final Project/fcfs.cpp
+++ Operating Systems Programming/Final Project/fcfs.cpp
@@ -8,8 +8,8 @@
 						 _.' `-' '._ _.' `-' '._ _.' `-' '._ _.' `-' '._
 						(.-./`-'\.-.|.-./`-'\.-.|.-./`-'\.-.|.-./`-`\.-.)
 						 `-'     `-' `-'     `-' `-'     `-' `-'     `-'
-	   					                   ...............
-							     	   |~ Formulas: ~|
+								   ...............
+								   |~ Formulas: ~|
 								   |RT = ST - AT |
 								   |TAT = CT - AT|
 								   |WT = TAT - BT|
@@ -31,11 +31,15 @@ struct process {
 
 // compares the arrival time between the processes
 bool compareArrivalTime(process p1, process p2)
-	{return p1.arrival_time < p2.arrival_time;}
+{
+	return p1.arrival_time < p2.arrival_time;
+}
 
 // compares the ID between the processes
 bool compareProcessID(process p1, process p2)
-	{return p1.p_id < p2.p_id;}
+{
+	return p1.p_id < p2.p_id;
+}
 
 // main function
 int main() {
@@ -43,17 +47,17 @@ int main() {
 	struct process p[50]; // 50 processes
 	int x;
 	int total_idle = 0,
-	    total_rt = 0,
-	    total_tat = 0,
-	    total_wt = 0;
+		total_rt = 0,
+		total_tat = 0,
+		total_wt = 0;
 	float avg_rt, // avg response time
-	      avg_tat, // avg turn around time
-		  avg_wt, // avg waiting time
-		  cpu_efficiency; // when cpu isn't idle
+		avg_tat, // avg turn around time
+		avg_wt, // avg waiting time
+		cpu_efficiency; // when cpu isn't idle
 
-	// by doing this, you can run either all 50 pro-
-	// cesses or you can run only the first couple
-	// for example 3 ... etc, however many you want
+   // by doing this, you can run either all 50 pro-
+   // cesses or you can run only the first couple
+   // for example 3 ... etc, however many you want
 	cout << "How many processes ? - ";
 	cin >> x; // prompt user for input
 
@@ -63,63 +67,81 @@ int main() {
 			infile >> p[i].p_id >> p[i].arrival_time >> p[i].burst_time;
 			i++; // dude. 
 		}
-		p[i].p_id = i + 1; 
+		p[i].p_id = i + 1;
 	}
-		sort(p, p + x, compareArrivalTime);
 
-		// calculating with formulas 
-		for (int i = 0; i < x; i++) { 
-			p[i].start_time = (i == 0) ? p[i].arrival_time : max(p[i - 1].end_time, p[i].arrival_time);
-			p[i].end_time = p[i].start_time + p[i].burst_time;
-			p[i].turnaround_time = p[i].end_time - p[i].arrival_time; 
-			p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
-			p[i].response_time = p[i].start_time - p[i].arrival_time;
+	// sort the processes based on
+	// the arrival time **********
+	sort(p, p + x, compareArrivalTime);
 
-			total_tat += p[i].turnaround_time;
-			total_wt += p[i].waiting_time;
-			total_rt += p[i].response_time;
-			total_idle += (i == 0) ? (p[i].arrival_time) : (p[i].start_time - p[i - 1].end_time);
-		}
+	// calculating with formulas 
+	for (int i = 0; i < x; i++) {
+		p[i].start_time = (i == 0) ? p[i].arrival_time : max(p[i - 1].end_time, p[i].arrival_time);
+		p[i].end_time = p[i].start_time + p[i].burst_time;
+		p[i].turnaround_time = p[i].end_time - p[i].arrival_time;
+		p[i].waiting_time = p[i].turnaround_time - p[i].burst_time;
+		p[i].response_time = p[i].start_time - p[i].arrival_time;
 
-		// calculating the average c: !
-		avg_tat = (float)total_tat / x;
-		avg_wt = (float)total_wt / x;
-		avg_rt = (float)total_rt / x;
-
-
-		cpu_efficiency = ((p[x].end_time - total_idle) / p[x].end_time) * 100 ;
-
-		sort(p, p + x, compareProcessID);
-
-		/*
-			OUTPUT !~ fun part hehe
-			layout plan : 
-			1. title 
-			2. total time, avg wt, cpu efficiency
-			3. Process 1:
-			   Service time = 
-			   Turnaround time =
-			   Process 2: ... etc
-		*/
-
-		cout << endl; // cute title UwU <3
-		cout << ":::::::::: ::::::::  :::::::::: ::::::::  " << endl;
-		cout << ":+:       :+:    :+: :+:       :+:    :+: " << endl;
-		cout << "+:+       +:+        +:+       +:+        " << endl;
-		cout << ":#::+::#  +#+        :#::+::#  +#++:++#++ " << endl;
-		cout << "+#+       +#+        +#+              +#+ " << endl;
-		cout << "#+#       #+#    #+# #+#       #+#    #+# " << endl;
-		cout << "###        ########  ###        ########  " << endl << endl << endl;
-
-		cout << "Total Time required is " << total_tat << " time units" << endl;
-		cout << "Average turn around time is " << avg_tat << " time units" << endl;
-		cout << "Average waiting time is " << avg_wt << " time units" << endl;
-		cout << "CPU Efficiency is " << cpu_efficiency << "%" << endl << endl;
-
-		// this loop prints the output
-		for (int i = 0; i < x; i++) {
-			cout << "Process " << p[i].p_id << ":\n";
-			cout << "Service time = " << p[i].burst_time << endl;
-			cout << "Turnaround time = " << p[i].turnaround_time << endl;
-		}
+		total_tat += p[i].turnaround_time;
+		total_wt += p[i].waiting_time;
+		total_rt += p[i].response_time;
+		total_idle += (i == 0) ? (p[i].arrival_time) : (p[i].start_time - p[i - 1].end_time);
 	}
+
+	// calculating the average c: !
+	avg_tat = (float)total_tat / x;
+	avg_wt = (float)total_wt / x;
+	avg_rt = (float)total_rt / x;
+
+
+	cpu_efficiency = ((p[x].end_time - total_idle) / p[x].end_time) * 100;
+
+	sort(p, p + x, compareProcessID);
+
+	/*
+		OUTPUT !~ fun part hehe
+		layout plan :
+		1. title
+		2. total time, avg wt, cpu efficiency
+		3. Process 1:
+		   Service time =
+		   Turnaround time =
+		   Process 2: ... etc
+	*/
+
+	cout << endl; // cute title UwU <3
+	cout << ":::::::::: ::::::::  :::::::::: ::::::::  " << endl;
+	cout << ":+:       :+:    :+: :+:       :+:    :+: " << endl;
+	cout << "+:+       +:+        +:+       +:+        " << endl;
+	cout << ":#::+::#  +#+        :#::+::#  +#++:++#++ " << endl;
+	cout << "+#+       +#+        +#+              +#+ " << endl;
+	cout << "#+#       #+#    #+# #+#       #+#    #+# " << endl;
+	cout << "###        ########  ###        ########  " << endl << endl << endl;
+
+	cout << "Total Time required is " << total_tat << " time units" << endl;
+	cout << "Average turn around time is " << avg_tat << " time units" << endl;
+	cout << "Average waiting time is " << avg_wt << " time units" << endl;
+	cout << "CPU Efficiency is " << cpu_efficiency << "%" << endl << endl;
+
+	// this loop prints the output
+	for (int i = 0; i < x; i++) {
+		cout << "Process " << p[i].p_id << ":\n";
+		cout << "Service time = " << p[i].burst_time << endl;
+		cout << "Turnaround time = " << p[i].turnaround_time << endl;
+	}
+	// save data to csv file
+	/*
+	ofstream toCSV;
+	string filename;
+	filename += "fcfsdata.csv";
+	cout << filename << endl;
+	toCSV.open(filename);
+	toCSV << "Process ID, Service Time, Turn Around Time" << endl;
+	for (int i = 0; i < x; i++) {
+		toCSV << p[i].p_id << ","
+			<< p[i].burst_time << ","
+			<< p[i].turnaround_time << endl;
+	}
+	toCSV.close();
+	*/
+}
