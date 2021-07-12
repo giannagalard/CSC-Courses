33530e917594ee14966675bd140377f54999cd7b
giannagalard <giannagalard@gmail.com>
2021-07-12 11:14:15 -0400

    Exercise Completed

---
+++ Interview Prep/Delete Duplicate-Value Nodes from a Sorted Linked List/solution.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'removeDuplicates' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+ 
+// time complexity -> O(n)
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    if(!llist) {
+        return llist;
+    }
+    
+    SinglyLinkedListNode* cur = llist->next;
+    SinglyLinkedListNode* prev = llist;
+    
+    while(cur != NULL) {
+        if(cur->data == prev->data) {
+            SinglyLinkedListNode* temp = cur;
+            cur = cur->next;
+            prev->next = cur;
+            delete temp;
+        }
+        else{
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return llist;
+}
+
+int main()
