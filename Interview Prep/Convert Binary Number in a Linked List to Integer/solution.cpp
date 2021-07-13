bcd86f9982b994006339274611115b8e908ed4a7
giannagalard <giannagalard@gmail.com>
2021-07-13 10:43:55 -0400

    exercise completed

---
+++ Interview Prep/Convert Binary Number in a Linked List to Integer/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// time complexity O(n)
+class Solution {
+    public:
+        int getDecimalValue(ListNode * head) {
+            int result = head -> val;
+            while (head -> next != NULL) { // check whats after head
+                result = result * 2 + head -> next -> val; // 1 * 2 + 0 =2, 2 * 2 + 0 = 4, 4 * 2 + 1 = 9, 9 *2 + 0 = 18
+                head = head -> next;
+            }
+            return result;
+        }
+};
